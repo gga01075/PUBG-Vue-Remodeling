649499416b23802543a08060b6029a01b34341a1
하상혁 <72422020+gga01075@users.noreply.github.com>
2022-01-13 16:45:48 +0900

    set router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import PageLayout from '@/pages/Layout/PageLayout'
+import Main from '@/pages/Main'
+import Patchnote from '@/pages/patchnote/Patchnote'
+import League from '@/pages/league/League'
+import Store from '@/pages/store/Store'
+import Community from '@/pages/community/Community'
+import Intro from '@/pages/intro/Intro'
+import Competition from '@/pages/competition/Competition'
 
 Vue.use(Router)
 
@@ -8,8 +16,45 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: PageLayout,
+      redirect: '/main',
+      children: [
+        {
+          path: '/main',
+          name: 'main',
+          component: Main
+        },
+        {
+          path: '/patchnote',
+          name: 'patchnote',
+          component: Patchnote
+        },
+        {
+          path: '/league',
+          name: 'league',
+          component: League
+        },
+        {
+          path: '/competition',
+          name: 'competition',
+          component: Competition
+        },
+        {
+          path: '/store',
+          name: 'store',
+          component: Store
+        },
+        {
+          path: '/community',
+          name: 'community',
+          component: Community
+        },
+        {
+          path: '/intro',
+          name: 'intro',
+          component: Intro
+        }
+      ]
     }
   ]
 })
