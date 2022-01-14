419ffae05d0077e08a12f748495e19cd59d38a19
하상혁 <72422020+gga01075@users.noreply.github.com>
2022-01-14 10:48:40 +0900

    modify routes and install eslint, prettier

---
+++ src/pages/Layout/PageLayout.vue
@@ -0,0 +1,26 @@
+<template>
+   <div class="wrap">
+
+   </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import SkipNav from "./SkipNav.vue";
+import Content from "./Content.vue";
+
+    export default({
+        components : {
+          Header, 
+          Footer,
+          SkipNav,
+          Content
+        },
+        data(){
+          
+        }
+
+    })
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
 import PageLayout from '@/pages/Layout/PageLayout'
 import Main from '@/pages/Main'
 import Patchnote from '@/pages/patchnote/Patchnote'
